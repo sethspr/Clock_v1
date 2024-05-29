a2de4e3f42c73f3dc470ef485d3a2c6d73379a7b
seth spring <seth.c.spring@gmail.com>
2024-05-28 21:01:43 -0400

    clock class working

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -3,5 +3,7 @@ public class Clock : MonoBehaviour {
     [SerializeField]
     Transform hoursPivot, minutePivot, secondsPivot;
 
-    void Awake () {}
+    void Awake () {
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+    }
 }
