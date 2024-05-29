ce9b64d8b1e938611a7d0ea5a9b5280825e5760b
seth spring <seth.c.spring@gmail.com>
2024-05-28 21:38:25 -0400

    continuous rotation current DateTime

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,10 +1,18 @@
 using System;
 using UnityEngine;
 public class Clock : MonoBehaviour {
-    [SerializeField]
-    Transform hoursPivot, minutePivot, secondsPivot;
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
 
-    void Awake () {
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30 * DateTime.Now.Hour);
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Update () {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = 
+            Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = 
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = 
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
     }
 }
